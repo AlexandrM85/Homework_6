7adc1a1313bb00382867bb76e57688c6d0d092dd
Alex <automex74@gmail.com>
2022-10-20 00:16:16 +0500

    add task_41

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("--------------Задача №41.--------------");
+int[] array = GetArray(8, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"Пользователь ввел чисел больше 0:  {PositiveCount(array)}.");
+
+int PositiveCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
