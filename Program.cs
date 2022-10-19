5d9d622896c4243ce8b444339838cc498b65fe9c
Alex <automex74@gmail.com>
2022-10-20 00:30:43 +0500

    add task_43

--- Program.cs
+++ Program.cs
@@ -25,4 +25,19 @@ int[] GetArray(int size, int minValue, int maxValue)
     return res;
 }
 
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("--------------Задача №43.--------------");
+Console.Write("Введите значение b1: ");
+double b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k1: ");
+double k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+double k2 = int.Parse(Console.ReadLine()!);
 
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых: {x}; {y}.");
